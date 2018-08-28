da72944fc3b5240bd8e7ba5b4105759e70dee676
Kandarp Khandwala <kandarpck@gmail.com>
2018-08-28 18:37:14 -0400

    26 - Remove duplicates from sorted array

---
+++ arrays/duplicates.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 2:
+            return len(nums)
+        replace = 0
+        for number in nums:
+            if number != nums[replace]:
+                replace += 1
+                nums[replace] = number
+        return replace + 1
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    ip = [1, 1, 2]
+    print(sol.removeDuplicates(ip))
+    print(ip)
