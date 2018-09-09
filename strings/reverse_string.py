478670e7185587a24e4d7fba8b6c00516876ad7c
Kandarp Khandwala <kandarpck@gmail.com>
2018-09-09 18:23:52 -0400

    Added generic method and complexity

--- strings/reverse_string.py
+++ strings/reverse_string.py
@@ -2,7 +2,7 @@ class Solution:
     def reverseString(self, s):
         """
         Time: N - iterate over elems
-        Space: 1
+        Space: N
         :type s: str
         :rtype: str
         """
@@ -17,9 +17,26 @@ class Solution:
             return s
         return self.reverseStringRecursive(s[len(s) // 2:]) + self.reverseStringRecursive(s[:len(s) // 2])
 
+    def reverseStringNormal(self, s):
+        """
+        Time: N
+        Space: N
+        :param s:
+        :return:
+        """
+        string_list = list(s)
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            string_list[i], string_list[j] = string_list[j], string_list[i]
+            i += 1
+            j -= 1
+        return "".join(string_list)
+
 
 if __name__ == '__main__':
     sol = Solution()
     ip = "hello"
     print(sol.reverseString(ip))
     print(sol.reverseStringRecursive(ip))
+    print(sol.reverseStringNormal(ip))
