2088dc195a158d5f08d8fb3520e8f033d54ce7d4
Kandarp Khandwala <kandarpck@gmail.com>
2018-09-09 21:38:11 -0400

    Find the first unique element in string - without Counter

--- strings/first_unique_char.py
+++ strings/first_unique_char.py
@@ -13,6 +13,20 @@ class Solution:
                 return i
         return -1
 
+    def firstUniqChar(self, s):
+        """
+        Time: N^2
+        Space: N
+        :type s: str
+        :rtype: int
+        """
+        checked = set()
+        for i, char in enumerate(s):
+            if char not in checked and s.count(char, i) == 1:
+                return i
+            checked.add(char)
+        return -1
+
 
 if __name__ == '__main__':
     sol = Solution()
@@ -22,4 +36,3 @@ if __name__ == '__main__':
     print(sol.firstUniqChar(ip))
     ip = "nononononnooon"
     print(sol.firstUniqChar(ip))
-
