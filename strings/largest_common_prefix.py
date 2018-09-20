b9450a4d5573bd55e57fd0abecfd39d4da218483
Kandarp Khandwala <kandarpck@gmail.com>
2018-09-19 22:59:47 -0400

    largest common prefix

---
+++ strings/largest_common_prefix.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        strs = sorted(strs)
+        first, last = strs[0], strs[-1]
+        while first:
+            if last.find(first) == 0:
+                return first
+            else:
+                first = first[:-1]
+        return ""
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    ip = ["flower", "flow", "flight"]
+    print(sol.longestCommonPrefix(ip))
