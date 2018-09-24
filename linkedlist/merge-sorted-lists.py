caf7e9688cf828d49e994729d7df8202a803a1e0
Kandarp Khandwala <kandarpck@gmail.com>
2018-09-24 00:39:19 -0400

    Merge 2 sorted lists

---
+++ linkedlist/merge-sorted-lists.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        if self is None:
+            return "Nil"
+        else:
+            return "{} -> {}".format(self.val, repr(self.next))
+
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        curr = dummy = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 or l2  # append the remaining part of the longer list
+        return dummy.next
+
+
+if __name__ == "__main__":
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+
+    head2 = ListNode(1)
+    head2.next = ListNode(3)
+    head2.next.next = ListNode(3)
+    head2.next.next.next = ListNode(5)
+    head2.next.next.next.next = ListNode(5)
+
+    print(Solution().mergeTwoLists(head, head2))
