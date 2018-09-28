cc3b06e3a7f0c376f17167731635cbd51fd7df13
Kandarp Khandwala <kandarpck@gmail.com>
2018-09-28 16:09:26 -0400

    Check if Linked List is a palindrome - add more examples

--- linkedlist/palindrome_ll.py
+++ linkedlist/palindrome_ll.py
@@ -43,6 +43,13 @@ if __name__ == "__main__":
 
     head = ListNode(1)
     head.next = ListNode(2)
+    head.next.next = ListNode(2)
+    head.next.next.next = ListNode(2)
+    head.next.next.next.next = ListNode(1)
+    print(Solution().isPalindrome(head))
+
+    head = ListNode(1)
+    head.next = ListNode(2)
     head.next.next = ListNode(3)
     head.next.next.next = ListNode(4)
     head.next.next.next.next = ListNode(5)
