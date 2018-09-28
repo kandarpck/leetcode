2d97e715dbe07c3336185815791dcb647dbff30c
Kandarp Khandwala <kandarpck@gmail.com>
2018-09-28 18:51:04 -0400

    Check is tree is valid BST

---
+++ trees/valid_bst.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return "{}".format(self.val)
+
+
+class Solution(object):
+    pass
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    print(Solution().maxDepth(root))
