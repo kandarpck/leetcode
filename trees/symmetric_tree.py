5e62c475c7dae9a66960c13fa73dfdbad915f5fc
Kandarp Khandwala <kandarpck@gmail.com>
2018-09-28 20:06:57 -0400

    Check is BST is symmetric around center

---
+++ trees/symmetric_tree.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return "{}".format(self.val)
+
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return False
+        stack = list()
+        stack.append(root.left)
+        stack.append(root.right)
+
+        while stack:
+            p, q = stack.pop(), stack.pop()
+
+            if not (p and q):
+                continue
+
+            if p is None or q is None or p.val != q.val:
+                return False
+
+            stack.append(p.left)
+            stack.append(q.right)
+
+            stack.append(p.right)
+            stack.append(q.left)
+        return True
+
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(2)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(5)
+    root.right.right = TreeNode(4)
+    print(Solution().isSymmetric(root))
+
+    root2 = TreeNode(1)
+    root2.left = TreeNode(2)
+    root2.right = TreeNode(2)
+    root2.left.left = TreeNode(4)
+    root2.left.right = TreeNode(5)
+    root2.right.left = TreeNode(6)
+    root2.right.right = TreeNode(4)
+    print(Solution().isSymmetric(root2))
