489fdef78e9f3207323f6332139831879a80ff6e
Kandarp Khandwala <kandarpck@gmail.com>
2018-10-02 23:44:35 -0400

    Modified test case

--- trees/level_order.py
+++ trees/level_order.py
@@ -30,5 +30,5 @@ if __name__ == "__main__":
     root = TreeNode(1)
     root.left = TreeNode(2)
     root.right = TreeNode(3)
-    root.left.left = TreeNode(4)
-    print(Solution().maxDepth(root))
+    root.right.right = TreeNode(4)
+    print(Solution().levelOrder(root))
