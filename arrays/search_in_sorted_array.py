96651ea4ecc17235ab374e54ebed9c0dd16ef516
Kandarp Khandwala <kandarpck@gmail.com>
2018-10-04 00:08:04 -0400

    Search in a sorted array

---
+++ arrays/search_in_sorted_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    nums = [4, 5, 6, 7, 0, 1, 2]
+    nums2 = [4, 5, 6, 7, 0, 1, 2]
+    print(sol.search(nums, 0))
+    print(sol.search(nums2, 3))
