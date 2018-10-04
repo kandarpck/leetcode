bd2c096b2e6314f577562dcabadb25e515350e6a
Kandarp Khandwala <kandarpck@gmail.com>
2018-10-03 23:58:45 -0400

    Boyer Moore Majority Vote

--- arrays/majority_element.py
+++ arrays/majority_element.py
@@ -16,9 +16,27 @@ class Solution:
                 res.append(num)
         return res
 
+    def majorityElement2(self, nums):
+        if not nums:
+            return []
+        count1, count2, candidate1, candidate2 = 0, 0, 0, 1
+        for n in nums:
+            if n == candidate1:
+                count1 += 1
+            elif n == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = n, 1
+            elif count2 == 0:
+                candidate2, count2 = n, 1
+            else:
+                count1, count2 = count1 - 1, count2 - 1
+        return [n for n in (candidate1, candidate2)
+                if nums.count(n) > len(nums) // 3]
+
 
 if __name__ == '__main__':
     sol = Solution()
     ip = [1, 1, 4, 4, 2, 2, 2, 1]
     print(sol.majorityElement(ip))
-    print(sol.majorityElement(ip))
+    print(sol.majorityElement2(ip))
