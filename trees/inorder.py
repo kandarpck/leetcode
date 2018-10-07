a5f14e5d29b80eb78ba5dd0985d3eb7b58eea51e
Kandarp Khandwala <kandarpck@gmail.com>
2018-10-06 23:56:36 -0400

    Define the class

--- trees/inorder.py
+++ trees/inorder.py
@@ -1,16 +1,25 @@
-def inorderTraversal(self, root):
-    """
-    :type root: TreeNode
-    :rtype: List[int]
-    """
-    order = []
-    self.inorderTraversalRecursive(order, root)
-    return order
+# Definition for a  binary tree node
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 
-def inorderTraversalRecursive(self, order, root):
-    if not root:
-        return
-    self.inorderTraversalRecursive(order, root.left)
-    order.append(root.val)
-    self.inorderTraversalRecursive(order, root.right)
+class Solution(object):
+
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        order = []
+        self.inorderTraversalRecursive(order, root)
+        return order
+
+    def inorderTraversalRecursive(self, order, root):
+        if not root:
+            return
+        self.inorderTraversalRecursive(order, root.left)
+        order.append(root.val)
+        self.inorderTraversalRecursive(order, root.right)
