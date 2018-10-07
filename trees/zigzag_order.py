07b748f3907b8ebd0a4f86a679273107de70a79b
Kandarp Khandwala <kandarpck@gmail.com>
2018-10-07 00:58:58 -0400

    Implement Zig zag order traversal

--- trees/zigzag_order.py
+++ trees/zigzag_order.py
@@ -15,12 +15,30 @@ class Solution:
         :type root: TreeNode
         :rtype: List[List[int]]
         """
+        result, current = [], [root]
+        counter = 0
+        while current:
+            counter += 1
+            next_level, vals = [], []
+            for node in current:
+                if node:
+                    vals.append(node.val)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            if counter % 2:
+                result.append(vals)
+            else:
+                result.append(list(reversed(vals)))
+            current = next_level
+        return result
 
 
 if __name__ == "__main__":
-    root = TreeNode(4)
+    root = TreeNode(1)
     root.left = TreeNode(2)
-    root.right = TreeNode(5)
-    root.left.left = TreeNode(1)
-    root.left.right = TreeNode(3)
+    root.right = TreeNode(3)
+    root.right.left = TreeNode(4)
+    root.right.right = TreeNode(5)
     print(Solution().zigzagLevelOrder(root))
