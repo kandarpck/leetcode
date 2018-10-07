17a06378fec1df2e66df439a05807a5b5dd5607e
Kandarp Khandwala <kandarpck@gmail.com>
2018-10-07 00:46:37 -0400

    Zig zag order traversal similar to level order

---
+++ trees/zigzag_order.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return "{}".format(self.val)
+
+
+class Solution:
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+
+
+if __name__ == "__main__":
+    root = TreeNode(4)
+    root.left = TreeNode(2)
+    root.right = TreeNode(5)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(3)
+    print(Solution().zigzagLevelOrder(root))
