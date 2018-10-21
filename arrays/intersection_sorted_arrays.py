e290908a012163e9d5eb3a38256678e83e89fc6d
Kandarp Khandwala <kandarpck@gmail.com>
2018-10-20 23:53:45 -0400

    return the intersection of sorted arrays

--- arrays/intersection_sorted_arrays.py
+++ arrays/intersection_sorted_arrays.py
@@ -4,3 +4,27 @@
 # B:   (2, 3, 3, 4, 9, 15)
 #
 # answer: (3, 3, 9)
+
+
+class Solution(object):
+    def intersection_sorted_arrays(self, A, B):
+        """
+        A, B: sorted input arrays
+        rtype: List
+        Time: O(max(a, b))
+        Space: 1
+        """
+        i, j = 0, 0
+        result = []
+        if not (A or B):
+            return result
+        while i < len(A) and j < len(B):
+            if A[i] < B[j]:
+                i += 1
+            elif B[j] < A[i]:
+                j += 1
+            else:
+                result.append(A[i])
+                i += 1
+                j += 1
+        return result
