444603c940f8cb72257a43e1416aa86d7dfe26a0
Kandarp Khandwala <kandarpck@gmail.com>
2018-11-02 22:31:55 -0700

    input list of string

---
+++ arrays/valid-tic-tac-toe.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def validTicTacToe(self, board):
+        """
+        :type board: List[str]
+        :rtype: bool
+        """
+        pass
+
+
+if __name__ == '__main__':
+    pass
