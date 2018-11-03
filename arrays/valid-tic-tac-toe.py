996b579c2f7fda3c2afff149ca79fb3ced15d269
Kandarp Khandwala <kandarpck@gmail.com>
2018-11-02 22:33:34 -0700

    check the win state

--- arrays/valid-tic-tac-toe.py
+++ arrays/valid-tic-tac-toe.py
@@ -15,6 +15,16 @@ class Solution(object):
             return (player == board[1][1] == board[0][0] == board[2][2] or
                     player == board[1][1] == board[0][2] == board[2][0])
 
+        FIRST, SECOND = ('X', 'O')
+        x_count = sum(row.count(FIRST) for row in board)
+        o_count = sum(row.count(SECOND) for row in board)
+        if o_count not in {x_count - 1, x_count}:
+            return False
+        if win(board, FIRST) and x_count - 1 != o_count:
+            return False
+        if win(board, SECOND) and x_count != o_count:
+            return False
+
 
 if __name__ == '__main__':
     pass
