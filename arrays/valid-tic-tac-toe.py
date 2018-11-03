014fe33e652a602d6bb9d664f26091e5e5072011
Kandarp Khandwala <kandarpck@gmail.com>
2018-11-02 22:32:40 -0700

    define win condition

--- arrays/valid-tic-tac-toe.py
+++ arrays/valid-tic-tac-toe.py
@@ -4,7 +4,16 @@ class Solution(object):
         :type board: List[str]
         :rtype: bool
         """
-        pass
+
+        def win(board, player):
+            for i in range(3):
+                if all(board[i][j] == player for j in xrange(3)):
+                    return True
+                if all(board[j][i] == player for j in xrange(3)):
+                    return True
+
+            return (player == board[1][1] == board[0][0] == board[2][2] or
+                    player == board[1][1] == board[0][2] == board[2][0])
 
 
 if __name__ == '__main__':
