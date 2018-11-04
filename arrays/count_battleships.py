7c15895b93abade8aaf720e83303f5f076921e34
Kandarp Khandwala <kandarpck@gmail.com>
2018-11-03 23:58:10 -0400

    implement the code

--- arrays/count_battleships.py
+++ arrays/count_battleships.py
@@ -4,7 +4,14 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: int
         """
-        pass
+        cnt = 0
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                cnt += int(board[i][j] == 'X' and
+                           (i == 0 or board[i - 1][j] != 'X') and
+                           (j == 0 or board[i][j - 1] != 'X'))
+        return cnt
+
 
 if __name__ == '__main__':
     pass
