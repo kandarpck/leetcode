bc0fb7e0cae4e9fb003c63370bf1904b152bc4f5
Kandarp Khandwala <kandarpck@gmail.com>
2018-11-04 23:41:25 -0500

    implemented the count the sequence and say it in words

--- strings/count_and_say.py
+++ strings/count_and_say.py
@@ -4,7 +4,21 @@ class Solution:
         :type n: int
         :rtype: str
         """
-        pass
+        seq = "1"
+        for i in range(n - 1):
+            seq = self.getNext(seq)
+        return seq
+
+    def getNext(self, seq):
+        i, next_seq = 0, ""
+        while i < len(seq):
+            cnt = 1
+            while i < len(seq) - 1 and seq[i] == seq[i + 1]:
+                cnt += 1
+                i += 1
+            next_seq += str(cnt) + seq[i]
+            i += 1
+        return next_seq
 
 
 if __name__ == '__main__':
