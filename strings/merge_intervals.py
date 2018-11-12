b1998bfec383139d050d02c14d4059b5453005d2
Kandarp Khandwala <kandarpck@gmail.com>
2018-11-11 23:50:07 -0500

    interval definition

--- strings/merge_intervals.py
+++ strings/merge_intervals.py
@@ -1,3 +1,10 @@
+# Definition for an interval.
+class Interval:
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
 class Solution:
     def merge(self, intervals):
         intervals.sort(key=lambda x: x.start)
