24be8ddfcb85afe0d4c9580c07415ae0063ee193
Kandarp Khandwala <kandarpck@gmail.com>
2019-01-12 23:05:56 -0500

    try again

--- sortandsearch/kth-largest.py
+++ sortandsearch/kth-largest.py
@@ -3,28 +3,28 @@ from random import randint
 
 class Solution(object):
     def findKthLargest(self, nums, k):
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            pivot_idx = randint(left, right)
-            new_pivot_idx = self.partion_around_pivot(left, right, pivot_idx, nums)
-            if new_pivot_idx == k - 1:
-                return nums[new_pivot_idx]
-            elif new_pivot_idx > k - 1:
-                right = new_pivot_idx - 1
-            else:  # new_pivot_idx < k - 1.
-                left = new_pivot_idx + 1
+        low, hi = 0, len(nums) - 1
+        while low <= hi:
+            rand_pivot = randint(low, hi)
+            pivot_idx = self.partition_around_pivot(low, hi, nums, rand_pivot)
+            if pivot_idx == k - 1:
+                return nums[pivot_idx]
+            elif pivot_idx < k - 1:
+                low = pivot_idx + 1
+            else:
+                hi = pivot_idx - 1
 
-    def partion_around_pivot(self, left, right, pivot_idx, nums):
-        pivot_value = nums[pivot_idx]
-        new_pivot_idx = left
-        nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
-        for i in range(left, right):
+    def partition_around_pivot(self, low, hi, nums, rand_pivot):
+        pivot_value = nums[rand_pivot]
+        pivot_idx = low
+        nums[hi], nums[rand_pivot] = nums[rand_pivot], nums[hi]
+        for i in range(low, hi):
             if nums[i] > pivot_value:
-                nums[i], nums[new_pivot_idx] = nums[new_pivot_idx], nums[i]
-                new_pivot_idx += 1
+                nums[i], nums[pivot_idx] = nums[pivot_idx], nums[i]
+                pivot_idx += 1
 
-        nums[right], nums[new_pivot_idx] = nums[new_pivot_idx], nums[right]
-        return new_pivot_idx
+        nums[hi], nums[pivot_idx] = nums[pivot_idx], nums[hi]
+        return pivot_idx
 
 
 nums = [9, 1, 4, 2, 3, 8, 7, 6]
