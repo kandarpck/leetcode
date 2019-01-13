274a495aea041324bd9d6c91b55c4296e346ef91
Kandarp Khandwala <kandarpck@gmail.com>
2019-01-12 21:35:20 -0500

    version 2 of top k freq elements problem using bucket sort

--- sortandsearch/top-k-freq.py
+++ sortandsearch/top-k-freq.py
@@ -15,6 +15,32 @@ class Solution(object):
         return [val for val, _ in nums_counter.most_common(k)]
 
 
+class Solution2(object):
+    # one pass dutch partitioning problem
+    # Time: O(n)
+    # Space: O(n)
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        nums_counter = Counter(nums)
+        buckets = [[] for _ in range(len(nums) + 1)]
+        for i, count in nums_counter.items():
+            buckets[count].append(i)
+
+        res = []
+        for i in reversed(range(len(buckets))):
+            if buckets[i]:
+                for j in range(len(buckets[i])):
+                    res.append(buckets[i][j])
+                    if len(res) == k:
+                        return res
+        return res
+
+
 if __name__ == '__main__':
-    nums = [0, 2, 2, 0, 1, 0, 2, 1]
+    nums = [0, 2, 2, 0, 1, 0, 2, 1, 3, 2, 2, 2]
     print(Solution().topKFrequent(nums, k=2))
+    print(Solution2().topKFrequent(nums, k=2))
