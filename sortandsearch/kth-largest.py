b95b3f316109f59965ae6f1ab497372573e41b35
Kandarp Khandwala <kandarpck@gmail.com>
2019-01-12 23:01:06 -0500

    kth largest element in array O(n) time

---
+++ sortandsearch/kth-largest.py
@@ -0,0 +1,31 @@
+from random import randint
+
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            pivot_idx = randint(left, right)
+            new_pivot_idx = self.partion_around_pivot(left, right, pivot_idx, nums)
+            if new_pivot_idx == k - 1:
+                return nums[new_pivot_idx]
+            elif new_pivot_idx > k - 1:
+                right = new_pivot_idx - 1
+            else:  # new_pivot_idx < k - 1.
+                left = new_pivot_idx + 1
+
+    def partion_around_pivot(self, left, right, pivot_idx, nums):
+        pivot_value = nums[pivot_idx]
+        new_pivot_idx = left
+        nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
+        for i in range(left, right):
+            if nums[i] > pivot_value:
+                nums[i], nums[new_pivot_idx] = nums[new_pivot_idx], nums[i]
+                new_pivot_idx += 1
+
+        nums[right], nums[new_pivot_idx] = nums[new_pivot_idx], nums[right]
+        return new_pivot_idx
+
+
+nums = [9, 1, 4, 2, 3, 8, 7, 6]
+print(Solution().findKthLargest(nums, k=3))
