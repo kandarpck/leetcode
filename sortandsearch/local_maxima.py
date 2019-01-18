93093bc4631061f34d1332b022e0da390d8ec081
Kandarp Khandwala <kandarpck@gmail.com>
2019-01-18 18:09:11 -0500

    Find peak element in list

---
+++ sortandsearch/local_maxima.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        low = 0
+        high = len(nums) - 1
+        while low < high:
+            m1 = (low + high) // 2
+            m2 = m1 + 1
+            if nums[m1 - 1] < nums[m1] and nums[m1] > nums[m2]:
+                return m1
+            if nums[m1] > nums[m2]:
+                high = m1
+            else:
+                low = m2
+        return low
+
+
+if __name__ == '__main__':
+    print(Solution().findPeakElement([1, 2, 3, 4, 5, 4, 3, 2, 1, 5, 7, 9, 11, 13, 12, 10, 8, 6, 4]))
